7680c05d5a18dc398a7dbe1303f1a468bad3b517
Anderson Parra <ander.pp@gmail.com>
2012-07-30 22:57:56 -0300

    add validacao restaurante

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120731011349) do
+ActiveRecord::Schema.define(:version => 20120731012213) do
 
   create_table "restaurantes", :force => true do |t|
-    t.string   "nome",       :limit => 80
+    t.string   "nome",          :limit => 80
     t.string   "endereco"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "especialidade", :limit => 40
   end
 
 end
