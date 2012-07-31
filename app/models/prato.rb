658c2a7e253cc3adf5901ce85088ca802af5c3ea
Anderson Parra <ander.pp@gmail.com>
2012-07-30 23:10:36 -0300

    add validacao prato

--- app/models/prato.rb
+++ app/models/prato.rb
@@ -1,2 +1,6 @@
+#encoding: utf-8
+
 class Prato < ActiveRecord::Base
+  validates_presence_of :nome, :message => "deve ser preenchido"
+  validates_uniqueness_of :nome, :message => "já cadastrado"
 end
