8c2b1de73e4737a9c6af91c24a613f4eb8fffd90
Anderson Parra <ander.pp@gmail.com>
2012-08-17 00:43:01 -0300

    qualificações a partir de um cliente ou um restaurante

--- app/controllers/qualificacoes_controller.rb
+++ app/controllers/qualificacoes_controller.rb
@@ -25,6 +25,14 @@ class QualificacoesController < ApplicationController
   # GET /qualificacoes/new.json
   def new
     @qualificacao = Qualificacao.new
+    
+    if params[:cliente]
+      @qualificacao.cliente = Cliente.find(params[:cliente])
+    end
+    
+    if params[:restaurante]
+      @qualificacao.restaurante = Restaurante.find(params[:restaurante])
+    end
 
     respond_to do |format|
       format.html # new.html.erb
